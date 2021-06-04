ded588c8795023a88cbc66ce53cc2fe3e2f9abc4
Ryan Maloney <rypmaloney@gmail.com>
2021-06-04 10:50:08 -0400

    refactored to use class declaration

--- scripts.js
+++ scripts.js
@@ -79,12 +79,14 @@ function removeChildNodes(parent) {
 }
 
 //book constructor 
-function book(title, author, pages, read) {
+class book {
+constructor(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
 }
+    }
 
 //add book to library array
 function addBookToLibrary(book) {
